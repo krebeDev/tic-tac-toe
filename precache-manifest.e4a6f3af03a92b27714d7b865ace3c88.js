04a09ae2a731cf01e8ce3475483e0526bcd2daf0
Solomon Ekrebe <krebedev@gmail.com>
2020-01-14 16:05:21 +0100

    Updates

---
+++ precache-manifest.e4a6f3af03a92b27714d7b865ace3c88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae4c2bdf461fdc0ac2d966a39338f82",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "38a0ad6c07518638dfcd",
+    "url": "/tic-tac-toe/static/css/main.a5438985.chunk.css"
+  },
+  {
+    "revision": "c215c7710ba9b421729e",
+    "url": "/tic-tac-toe/static/js/2.fa9cdbb6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.fa9cdbb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38a0ad6c07518638dfcd",
+    "url": "/tic-tac-toe/static/js/main.eef19222.chunk.js"
+  },
+  {
+    "revision": "62a4ca9edeeeb2a1e5f0",
+    "url": "/tic-tac-toe/static/js/runtime-main.50f506ba.js"
+  }
+]);
